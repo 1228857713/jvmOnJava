3a3ad86ca9fc12539b222b8caadd0dc6159a06a5
王震 <1228857713@qq.com>
2021-02-18 16:08:36 +0800

    提交答案

--- algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_239/Solution.java
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_239/Solution.java
@@ -7,6 +7,26 @@ package com.wangzhen.algorithm.leetcode.lt_239;
  */
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+        int [] result = new int[nums.length-k+1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = maxNum(nums,i,i+k);
+        }
+        return result;
     }
+
+
+    /**
+     * @desc 求最大值
+     * @param nums
+     * @return
+     */
+    public int maxNum(int  []nums,int start ,int end){
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < end; i++) {
+            if(nums[i]>max)
+                max = nums[i];
+        }
+        return max;
+    }
+
 }
