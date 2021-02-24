fd3948382fdea6213c58d9137827aaa1cc408db5
wangzhen <1228857713@qq.com>
2021-02-24 20:34:44 +0800

    add jvm 视频

---
+++ java-study/src/main/java/com/wangzhen/jvmTest/other/CLassInit.java
@@ -0,0 +1,20 @@
+package com.wangzhen.jvmTest.other;
+
+/**
+ * Description: 对象初始化与类初始化相关内容
+ * Datetime:    2021/2/22   下午4:32
+ * Author:   王震
+ */
+public class CLassInit {
+    {
+        int a = 1;
+    }
+    static {
+        int b = 2;
+    }
+    int c =3;
+    static  int d =4;
+    static final int e =5;
+    Object object= new Object();
+    static Object object2 = new Object();
+}
